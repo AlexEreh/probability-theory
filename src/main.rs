c0da45925c02c0424eb43a0ddc86030d2e3036b0
Alexander Ereshkin <xcode111@mail.ru>
2023-09-23 01:10:46 +0300

    Add wait for user input and change expects to unwraps.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 extern crate num;
 
+use std::io;
+use std::io::{Read, Write};
 use num::{BigUint, ToPrimitive};
 use num::rational::Ratio;
 
 fn factorial(n: usize) -> BigUint {
+    if n == 0 {
+        return BigUint::from(1usize);
+    }
+
     let mut a = BigUint::from(1usize);
 
     for i in 2..n {
@@ -14,15 +20,22 @@ fn factorial(n: usize) -> BigUint {
 }
 
 fn get_probability(x: usize) -> Ratio<BigUint> {
-    let first_multiplier = Ratio::from((factorial(90), factorial(100)));
+    let first_multiplier = Ratio::from(
+        (factorial(90), factorial(100))
+    );
 
-    let second_multiplier = Ratio::from((factorial(100 - x), factorial(90 - x)));
+    let second_multiplier = Ratio::from(
+        (factorial(100 - x), factorial(90 - x))
+    );
     return Ratio::from(BigUint::from(1usize)) - (first_multiplier * second_multiplier);
 }
 
 fn main() {
     println!("x  | P ");
-    for num in 0..90 {
-        println!("{:02} | {1:.14}", num, get_probability(num).to_f64().expect(""))
+    for num in 0..91 {
+        println!("{:02} | {1:.14}", num, get_probability(num).to_f64().unwrap_or(f64::NAN))
     }
+    println!("Нажмите Enter чтобы выйти.");
+    io::stdout().flush().unwrap();
+    io::stdin().read(&mut [0]).unwrap();
 }
